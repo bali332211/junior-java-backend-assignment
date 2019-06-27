f5ddeecc3c6850249fc94f837fa8105f64ecedf2
Bali <bali332211@freemail.hu>
2019-06-27 12:27:17 +0200

    Add main page htmls, css, controller and dependencies

---
+++ src/main/java/io/falcon/assignment/controllers/MainController.java
@@ -0,0 +1,13 @@
+package io.falcon.assignment.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+  @GetMapping("/")
+  public String main() {
+    return "main";
+  }
+}
